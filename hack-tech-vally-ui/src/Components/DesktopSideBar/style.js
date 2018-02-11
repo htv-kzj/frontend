95548a57782de123e531f163641af2b88e3aa4cf
Sean Dufort <Sean.Dufort@ge.com>
2018-02-10 21:32:52 -0500

    Update desktop side bar style and add nav bar skeleton

--- hack-tech-vally-ui/src/Components/DesktopSideBar/style.js
+++ hack-tech-vally-ui/src/Components/DesktopSideBar/style.js
@@ -1,7 +1,23 @@
+import GlobalStyle from '../../style.js';
+
+const { fontColor, busColor, iconColor, themeColor } = GlobalStyle;
+
 const Style = {
-  colorBlue: {
-    color: 'blue',
+  headerStyle: {
+    backgroundColor: themeColor,
+    paddingTop: 50,
+    paddingBottom: 50,
+    alignItems: 'center',
   },
+  busStyle: {
+    height: 50,
+    width: 50,
+    paddingTop: 5,
+  },
+  titleStyle: {
+    color: "white",
+    textAlign: 'center',
+  }
 }
 
 export default Style;
